699b621ede56d44fbc9c9b6a982ddf6d6f9ac2f3
Evgesha <kydinova1712@gmail.com>
2024-11-27 15:13:55 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+total_tasks = 12
+hours_spent = 1.5
+cours_name = 'Python'
+average_time = (hours_spent / total_tasks)
+print('Курс:', cours_name, ', всего задач:', total_tasks, ', затрачено часов:', hours_spent, ', среднее время выполнения', average_time, 'часа')
